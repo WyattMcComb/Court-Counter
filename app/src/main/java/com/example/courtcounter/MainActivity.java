7f94635b1c34a2b345ea34387ea371f809c12070
Wyatt McComb <02wyatt.mccomb@gulllakecs.org>
2021-02-15 12:51:02 -0500

    Made the 3,2, and 1 buttons work

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamA(View v){
+        displayForTeamA(3);
+    }
+
+    public void addTwoForTeamA(View v){
+        displayForTeamA(2);
+    }
+
+    public void addOneForTeamA(View v){
+        displayForTeamA(1);
     }
 }
