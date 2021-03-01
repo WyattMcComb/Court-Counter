d7244a6fb4754343e93264acbb74dcb34b24d337
Wyatt McComb <02wyatt.mccomb@gulllakecs.org>
2021-03-01 12:50:15 -0500

    Made The Team B Buttons Work

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,4 +39,28 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA = scoreTeamA +1;
         displayForTeamA(scoreTeamA);
     }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamB(View v){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View v){
+        scoreTeamB = scoreTeamB +2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View v){
+        scoreTeamB = scoreTeamB +1;
+        displayForTeamB(scoreTeamA);
+    }
+
 }
